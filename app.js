925fdc2b137eec8b90f05b586015d1c8f1677980
woomin <ksaw1228@gmail.com>
2023-07-05 10:10:29 +0900

    secu path2

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
-const path = require('path');
 const axios = require('axios');
-const cors = require('cors'); //var
+const cors = require('cors');
 const rateLimit = require("express-rate-limit");
+
+let seasonStarted = false
+let season = 2023
 
 require('dotenv').config();
 
@@ -17,7 +19,6 @@ const apiLimiter = rateLimit({
 app.use("/", apiLimiter)
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use( '/', express.static( path.join(__dirname, 'public') ))
 app.use(cors());
 
 
@@ -28,17 +29,17 @@ MongoClient.connect(process.env.MONGO_DB_URL,(err,client) => {
     app.listen(process.env.PORT,()=>{
         console.log(process.env.PORT+"on")
         getData();
-        // setInterval(getData, 1000 * 60 * 60 * 1);
+        setRealTimeUpdate(seasonStarted,1)
     })
 })
 
 function getData() {
   const leagues = ["PL", "BL1", "FL1", "PD", "SA"];
-  leagues.map(updateDB);
+  leagues.map(leagueName=>updateDB(leagueName,season));
 }
 
-async function updateDB(leagueName){
-    const url = `https://api.football-data.org/v4/competitions/${leagueName}/matches?season=2023`
+async function updateDB(leagueName,season){
+    const url = `https://api.football-data.org/v4/competitions/${leagueName}/matches?season=${season}`
     const headers = { "X-Auth-Token":process.env.API_KEY}
     try {
       const response = await axios.get(url, { headers: headers });
@@ -69,23 +70,30 @@ async function updateDB(leagueName){
 async function newSeasonInsert(leagueName,season){
   const url = `https://api.football-data.org/v4/competitions/${leagueName}/matches?season=${season}`
   const headers = { "X-Auth-Token": API_KEY}
-
-  const response = await axios.get(url, { headers: headers });
-  const matches = response.data.matches
-
-  for(let i=0; i<matches.length; i++){
-    db.collection(leagueName).insertOne({
-      id: matches[i].id,
-      Date: matches[i].utcDate,
-      MatchDay: matches[i].matchday,
-      Home: matches[i].homeTeam.shortName,
-      HomeIco: matches[i].homeTeam.crest,
-      Away: matches[i].awayTeam.shortName,
-      AwayIco: matches[i].awayTeam.crest,
-      Score: `${matches[i].score.fullTime.home}:${matches[i].score.fullTime.away}`,
-    })
+  try{
+    const response = await axios.get(url, { headers: headers });
+    const matches = response.data.matches
+  
+    for(let i=0; i<matches.length; i++){
+      db.collection(leagueName).insertOne({
+        id: matches[i].id,
+        Date: matches[i].utcDate,
+        MatchDay: matches[i].matchday,
+        Home: matches[i].homeTeam.shortName,
+        HomeIco: matches[i].homeTeam.crest,
+        Away: matches[i].awayTeam.shortName,
+        AwayIco: matches[i].awayTeam.crest,
+        Score: `${matches[i].score.fullTime.home}:${matches[i].score.fullTime.away}`,
+      })
+  }
+  console.log(leagueName+" New season inserted!")
+  }catch(error){
+    console.log(error)
+  }
 }
-console.log(leagueName+" New season inserted!")
+
+function setRealTimeUpdate(seasonStart,updateCycleHour){
+  if(seasonStart === true) setInterval(getData, 1000 * 60 * 60 * updateCycleHour);
 }
 
 app.get("/api/all", async (req, res) => {
@@ -109,7 +117,7 @@ app.get("/api/all", async (req, res) => {
 app.get('/api/:id',async (req,res) => {
   const reqLeague = req.params.id
   //데이터검증
-  if(reqLeague.length > 30){
+  if(reqLeague.length > 20){
     res.send(500)
   }else{
     const leagues = reqLeague.split('+')
